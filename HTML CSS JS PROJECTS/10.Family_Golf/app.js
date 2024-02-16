83adeea2e044a104e699b65419429c113a8b7f7c
Ashu <ashuprince7337@gmail.com>
2024-02-16 19:35:57 +0530

    max upload

---
+++ HTML CSS JS PROJECTS/10.Family_Golf/app.js
@@ -0,0 +1,24 @@
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "110px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
+        start:"top -10%",
+        end: "top -11%",
+        scrub: true,
+    }
+})
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -50%",
+        end: "top -100%",
+        scrub:2,
+    }
+})
