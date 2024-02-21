c6a247a6c3f1ff668346b4505fbca9c7844e5dca
Ashu <ashuprince7337@gmail.com>
2024-02-21 08:03:58 +0530

    complete GSAP Updated

---
+++ HTML CSS JS PROJECTS/08.GSAP/05.Staggers/app.js
@@ -0,0 +1,16 @@
+gsap.set(".box", {
+    borderRadius:0,
+})
+gsap.to(".box", {
+    borderRadius: 100,
+    duration: 0.5,
+    y: -100,
+    ease: "power1",
+    yoyo:true,
+    repeat:-1,
+    stagger: {
+        amount:1,
+        each:0.5,
+        from:3,
+    }
+})
