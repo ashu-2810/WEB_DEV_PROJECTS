0094d60c3b9fc5a5b9e991942a3da92dc88e1bd3
Ashu <ashuprince7337@gmail.com>
2024-06-09 22:41:37 +0530

    09-06-2024

--- REACT_PROJECTS/01-project/src/App.jsx
+++ REACT_PROJECTS/01-project/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
+import Hero from './components/Hero';
 
 const App = () => {
     return (
         <>
             <Navigation />
-            
+            <Hero />
         </>
     )
 }
