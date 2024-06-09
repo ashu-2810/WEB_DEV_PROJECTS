58f0382aabc5a72e2c0aaea8630d6ac901eed510
Ashu <ashuprince7337@gmail.com>
2024-06-09 14:00:04 +0530

    09-06-2024

---
+++ REACT_PROJECTS/01-project/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h1 className='m-0 w-full p-2 flex justify-center items-center text-orange-600 '>hello</h1>
+    </>
+  )
+}
+
+export default page
