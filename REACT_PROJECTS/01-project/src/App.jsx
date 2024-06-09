482c9ec24e0ab528ba6889a776e38ce9670d9c15
Ashu <ashuprince7337@gmail.com>
2024-06-09 18:08:53 +0530

    09-06-2024

---
+++ REACT_PROJECTS/01-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+
+const App = () => {
+    return (
+        <>
+            <Navigation />
+            
+        </>
+    )
+}
+
+export default App;
